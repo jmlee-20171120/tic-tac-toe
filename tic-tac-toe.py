272659c1bc5af7c8f96188942908b3f9ed212bf3
jmlee-20171120 <33817326+jmlee-20171120@users.noreply.github.com>
2017-11-20 11:00:15 +0900

    Implement victory conditions and delete redundant code

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -6,8 +6,8 @@ def checked(i) :
 
       if button["text"] != "     " :
             return
+
       button["text"] = player 
-      button["bg"] = "yellow"
 
       if player == "X" :
             player = "O"
@@ -15,6 +15,23 @@ def checked(i) :
       else :
             player = "X"
             button["bg"] = "lightgreen"
+
+      row = i // 3
+      col = i % 3
+
+      # Three-in-a-row
+      if list[row * 3]["text"] == list[row * 3 + 1]["text"] == list[row * 3 + 2]["text"] :
+            display_win()
+      # Three-in-a-column
+      elif list[col]["text"] == list[col + 3]["text"] == list[col + 6]["text"] :
+            display_win()
+      # Diagonals
+      elif list[0]["text"] == list[4]["text"] == list[8]["text"] != "     " or list[2]["text"] == list[4]["text"] == list[6]["text"] != "     " :
+            display_win()
+
+def display_win() :
+      msg = Message(window, text="You Win!")
+      msg.grid(row=3, column=0)
 
 window = Tk()
 player = "X"
@@ -26,5 +43,3 @@ for i in range(9) :
       list.append(b)
 
 window.mainloop()
-
-
